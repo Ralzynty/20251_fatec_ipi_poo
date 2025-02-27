b4ef5b43adb47e3fd963d06cf118f3390634e9a9
Natalia Ike <naty.ike0116@gmail.com>
2025-02-27 18:16:34 -0300

    feat: implementa o menu

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -3,3 +3,12 @@ def entradaValores():
     a = int(input("Digite o primeiro número da operação: "))
     b = int(input("Digite o segundo número da operação: "))
     return a, b
+
+def menu():
+    while  True:
+        print("\nEscolha uma opção:")
+        print("1. Somar")
+        print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
